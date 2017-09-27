4385c02dfcdaefecd8cbe3b32c228738f483fb86
peerum.you@campjefferson.com <peerum.you@ws-to-wm-p4658.identification7.com>
2017-09-27 09:40:16 -0400

    added react toolbox

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
 import style from './style';
+import { Button } from 'react-toolbox/lib/button';
 
 export default class Home extends Component {
 	render() {
@@ -7,6 +8,7 @@ export default class Home extends Component {
 			<div class={style.home}>
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
+				<Button label="Hello World!" />
 			</div>
 		);
 	}
