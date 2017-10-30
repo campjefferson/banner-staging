679d81ad3bbd5268e02d6a52150fcd3085fc23ab
Peerum You <peerum.you@campjefferson.com>
2017-10-30 14:29:44 -0400

    styling for View page

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -3,6 +3,14 @@ import { Button, IconButton } from "react-toolbox/lib/button";
 
 import style from "./style";
 
+const InfoBar = () => (
+  <div class={style.infoBar}>
+    <IconButton icon="info" flat inverse class={style.icon} />
+    <IconButton icon="replay" flat inverse class={style.icon} />
+    <IconButton icon="file_download" flat inverse class={style.icon} />
+  </div>
+);
+
 export default class Viewer extends Component {
   // gets called when this route is navigated to
   componentDidMount() {}
@@ -11,8 +19,20 @@ export default class Viewer extends Component {
   render({ project, toggleDrawerActive }) {
     return (
       <div class={style.view}>
-        <h1>Project: {project}</h1>
-        <Button label="open drawer" onClick={toggleDrawerActive} />
+        <div class={style.subnav}>
+          <div>
+            <h1>Project: {project}</h1>
+          </div>
+          <div class={style.liveDate}>
+            <span class={style.title}>Live date:</span>
+            <span class={style.date}>Oct 31, 2017</span>
+          </div>
+        </div>
+        <div class={style.body}>
+          <Button label="menu" onClick={toggleDrawerActive} class={style.menuButton} />
+          <iframe style="width:300px;height:250px;border:1px solid black;" />
+        </div>
+        <InfoBar />
       </div>
     );
   }
